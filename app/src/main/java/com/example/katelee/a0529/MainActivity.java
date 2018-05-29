c417f78d82084a12e03cd568f6db22056842745f
이연주 <katelee@iyeonjuui-MacBook-Pro.local>
2018-05-29 15:23:16 +0900

    Initial commit

---
+++ app/src/main/java/com/example/katelee/a0529/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.katelee.a0529;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView myButton;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        myButton = findViewById(R.id.myButton)
+    }
+
+    public void myButtonFunction(View view){
+
+    }
+}
